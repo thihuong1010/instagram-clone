21dfbcf1353f4f3322e79ef58f65d5ec92ca2b4c
Huong Nguyen <huongnguyen10102@gmail.com>
2021-11-16 18:41:30 +0200

    add props for post

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
       <div className="app__header">
         <img className="app__headerImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2880px-Instagram_logo.svg.png"
         alt=""/>
-    </div>
+      </div>
       <h1>instagram clone by Huong Nguyen</h1>
-
-    <Post/>
-    {/* post */}
-
+      <Post username="mina4873" caption="lovelyyy" imageUrl="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1480&q=80"/>
+      <Post username="tunti34" caption="i love react" imageUrl="https://images.ctfassets.net/51xdmtqw3t2p/2w0H06U9MYaJNsonXhyD3I/0cd72a4b4e01460bcd7145e984b05c38/Portada_react.jpg?w=1280&q=50"/>
+      <Post username="lulu00" caption="uhu" imageUrl="https://images.unsplash.com/photo-1597589827317-4c6d6e0a90bd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c3F1YXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"/>
+      <Post username="ssussu" caption="lalala let sing" imageUrl="https://images.unsplash.com/photo-1516475429286-465d815a0df7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyYWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"/>
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './Post.css';
 import Avatar  from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({username, caption, imageUrl}) {
     return (
-        <div>
-            <h3>username</h3>
-            {/* avatar */}
-            <Avatar 
-            />
-            {/* img */}
-            <img className="post__image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1920px-Instagram_logo_2016.svg.png" alt=""/>
-            {/* username + caption */}
-            <h4 className="post__text"><strong>username</strong> caption</h4>
+        <div className="post">
+            <div className="post__header">
+                <Avatar className="post__avatar"
+                alt=""
+                src=""
+                />
+                <h3>{username}</h3>
+            </div>
+            <img className="post__image" src={imageUrl} alt=""/>
+            <h4 className="post__text"><strong>username</strong> {caption}</h4>
         </div>
     )
 }

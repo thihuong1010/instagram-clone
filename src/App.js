5d8f220a39120717b7186cf50b73f2a4e486cadb
Huong Nguyen <huongnguyen10102@gmail.com>
2021-12-27 00:00:33 +0200

    sign up form in model

--- src/App.js
+++ src/App.js
@@ -2,9 +2,37 @@ import {useEffect, useState } from 'react';
 import './App.css';
 import Post from './Post'; 
 import { db } from './firebase';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Input from '@material-ui/core/Input';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
 
   // UseEffect run a piece of code based on a specific condition
 
@@ -18,8 +46,39 @@ function App() {
     })
   }, []); 
 
+  const signUp = (event) => {
+   
+  }
+
   return (
     <div className="App">
+
+      <div>
+        <Button onClick={handleOpen}>Sign up</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+        >
+          <Box sx={{ ...style, width: 400 }}>
+            <center>
+              <img className="app__headerImage" 
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2880px-Instagram_logo.svg.png"
+              alt=""/>
+              <Input type="text" value={username} placeholder="username"
+              onChange={(e) => setUsername(e.target.value)}
+              />
+              <Input type="text" value={email} placeholder="emai"
+              onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input type="text" value={password} placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button onClick={signUp}>Sign up</Button>
+            </center>
+          </Box>
+        </Modal>
+      </div>
+
       <div className="app__header">
         <img className="app__headerImage" 
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2880px-Instagram_logo.svg.png"

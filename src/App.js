74892ba1a7f0ca2de739dd31876ee6bafe8dbd4d
Huong Nguyen <huongnguyen10102@gmail.com>
2021-12-26 18:00:11 +0200

    add individual id

--- src/App.js
+++ src/App.js
@@ -11,21 +11,25 @@ function App() {
   useEffect( () => {
     db.collection('posts').onSnapshot(snapshot => {
       // every time a new post is added,...
-      setPosts(snapshot.docs.map(doc => doc.data()));
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
     })
   }, []); 
 
   return (
     <div className="App">
       <div className="app__header">
-        <img className="app__headerImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2880px-Instagram_logo.svg.png"
+        <img className="app__headerImage" 
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2880px-Instagram_logo.svg.png"
         alt=""/>
       </div>
       <h1>instagram clone by Huong Nguyen</h1>
 
     {
-      posts.map((post, index) => (
-        <Post key={index} username={post.username} 
+      posts.map(({id, post}) => (
+        <Post key={id} username={post.username} 
         caption={post.caption} 
         imageUrl={post.imageUrl}/>
       ))
